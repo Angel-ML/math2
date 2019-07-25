87008cdfb111af8426d84875282af459bf578b61
fitzwang <fitzwang.ml@gmail.com>
2019-07-25 13:40:47 +0800

    the first commit

---
+++ src/main/java/com/tencent/angel/ml/math2/utils/MathException.java
@@ -0,0 +1,21 @@
+package com.tencent.angel.ml.math2.utils;
+
+
+public class MathException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
+  public MathException() {
+  }
+
+  public MathException(String message) {
+    super(message);
+  }
+
+  public MathException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public MathException(Throwable cause) {
+    super(cause);
+  }
+}
